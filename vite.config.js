74a8f7f440d3d4e967a059ae66a6239ed7ec6b85
ShahrazadMasalmah <146379471+ShahrazadMasalmah@users.noreply.github.com>
2024-02-26 15:50:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-   plugins: [
-    process.env.MODE !== 'production' ? react({
-      jsxRuntime: 'classic',
-    }) : react(),
-    svgLoader(),
-  ],
+  plugins: [react({ jsxRuntime: 'classic' })],
 })
